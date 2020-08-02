35b7314bf2567310534a175da7bd12886a89b1a7
Bowen Zhang <bowenzhang@gmail.com>
2020-08-02 19:07:30 -0400

    Functional post and get routes

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const app = express();
+app.use(express.json());
 
 let codeSchema = new mongoose.Schema({
     hotelName: {
         type: String,
-        required: 'This field is required!'
+        required: true
     },
     codes: [String]
 });
@@ -17,11 +18,12 @@ const Code = mongoose.model('Code', codeSchema);
 mongoose.connect('mongodb://localhost:27017/hoteldb', {useNewUrlParser: true, useUnifiedTopology:  true});
 
 const port = process.env.PORT || 8080;
+
 app.listen(port, ()=> {
     console.log(`Listening on port ${port}`);
 });
 
-app.get('/', cors(), async (req, res) => {
+app.get('/', async (req, res) => {
     let hotelName = req.body.hotelName;
 
     const codes = await Code.find({
@@ -32,12 +34,21 @@ app.get('/', cors(), async (req, res) => {
     res.send(codes);
 });
 
-app.post('/', cors(), async (req, res) => {
-    let newHotel = new Code({
-        'hotelName': req.body.name,
-        'codes': req.body.codes
-    });
-    
-    newHotel = await newHotel.save();
-    res.send(newHotel);
+app.post('/', async (req, res) => {   
+    try {
+        let newHotel = new Code({ 
+            hotelName: req.body.hotelName,
+            codes: req.body.codes
+        });
+        newHotel = await newHotel.save();
+        res.send(newHotel);
+    } catch(error) {
+        res.send(error.message);
+    }
 });
+
+// todo: 
+// test post
+// test get
+// make put
+// test put
