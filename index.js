c368de21e7eb86d9b2463b74190c6060b0009f16
Bowen Zhang <bowenzhang@gmail.com>
2020-08-02 17:38:20 -0400

    added get and post routes

--- index.js
+++ index.js
@@ -14,9 +14,30 @@ let codeSchema = new mongoose.Schema({
 
 const Code = mongoose.model('Code', codeSchema);
 
-mongoose.connect('mongodb://localhost:27017/hoteldb.codes');
+mongoose.connect('mongodb://localhost:27017/hoteldb', {useNewUrlParser: true, useUnifiedTopology:  true});
 
 const port = process.env.PORT || 8080;
 app.listen(port, ()=> {
     console.log(`Listening on port ${port}`);
+});
+
+app.get('/', cors(), async (req, res) => {
+    let hotelName = req.body.hotelName;
+
+    const codes = await Code.find({
+        'hotelName': hotelName
+    });
+
+    if (!codes) return res.status(404).send('The specified hotel was not found');
+    res.send(codes);
+});
+
+app.post('/', cors(), async (req, res) => {
+    let newHotel = new Code({
+        'hotelName': req.body.name,
+        'codes': req.body.codes
+    });
+    
+    newHotel = await newHotel.save();
+    res.send(newHotel);
 });
