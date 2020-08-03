be16e9bc9d152d99952d8b0b4ea4508565deb2c6
Bowen Zhang <bowenzhang@gmail.com>
2020-08-02 20:04:51 -0400

    deleted unused file

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-
 mongoose.connect('mongodb://localhost:27017/hoteldb', {useNewUrlParser: true, useUnifiedTopology:  true});
 
 app.use(express.json());
