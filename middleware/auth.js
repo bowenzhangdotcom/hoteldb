897e20b5efe7fd2270d614235df6809919eedc37
Bowen Zhang <bowenzhang@gmail.com>
2020-08-03 21:08:38 -0400

    Added auth via .env variables

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,33 @@
 const basicAuth = require('express-basic-auth');
+require('dotenv').config();
 
-const userAuth = basicAuth({
-    users: {'user': 'testuser'}
+const getUnauthRes = (req) => {
+    return req.auth
+    ? (`Credentials ${req.auth.user} : ${req.auth.password} are invalid`)
+    : 'No credentials provided'
+};
+
+function generalAuthorizer(username, password) {
+    const userMatches = basicAuth.safeCompare(username, process.env.generalName);
+    const passwordMatches = basicAuth.safeCompare(password, process.env.generalPassword);
+    return userMatches && passwordMatches || adminAuthorizer(username, password);
+}
+
+function adminAuthorizer(username, password) {
+    const userMatches = basicAuth.safeCompare(username, process.env.adminName);
+    const passwordMatches = basicAuth.safeCompare(password, process.env.adminPassword);
+    return userMatches && passwordMatches
+}
+
+const generalAuth = basicAuth({
+    authorizer: generalAuthorizer,
+    unauthorizedResponse: getUnauthRes
 });
 
 const adminAuth = basicAuth({
-    users: {'admin': 'testadmin'}
+    authorizer: adminAuthorizer,
+    unauthorizedResponse: getUnauthRes
 });
 
-exports.userAuth = userAuth;
+exports.generalAuth = generalAuth;
 exports.adminAuth = adminAuth;
